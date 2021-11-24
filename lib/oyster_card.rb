3cb5fb7be69c7e8367a0b33637a08932c58dc34d
KKassim <k_master@hotmail.co,uk>
2021-11-24 13:31:39 +0000

    creates describe for touch_in/top_up methods and input doubles (still unused)

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -5,28 +5,30 @@ class OysterCard
   FARE = 1
   MINIMUM_FARE = -1
   MAX_BALANCE = 90
-  attr_reader :balance, :max, :in_journey, :entry_station
+  ERROR = {
+    max: 'Maximum allowance reached'.freeze,
+    min: 'insufficient funds'
+   }
+  attr_reader :balance, :in_journey, :entry_station
 
-  def initialize(max = MAX_BALANCE, min = MINIMUM_FARE, _fare = FARE)
-    @balance = 10
-    @max = max
+  def initialize
+    @balance = 0
     @in_journey = false
-    @min = min
-    @entry_station = []
+    @entry_station = nil
   end
 
   def top_up(amount = 0)
-    raise "You already have £#{MAX_BALANCE}!" if @balance + amount > @max
+    raise ERROR[:max] if @balance + amount > MAX_BALANCE
 
     @balance += amount
   end
 
-  def deduct(_fare = 1)
+  def deduct(fare = 0)
     @balance += MINIMUM_FARE
   end
 
   def touch_in
-    raise 'insufficient funds' if @balance < MINIMUM_FARE
+    raise 'insufficient funds' if @balance < FARE
 
     @in_journey = true
     # entry_station = station
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -3,30 +3,55 @@
 require 'oyster_card'
 
 describe OysterCard do
+
+  let(:station){ double :station }
+
   it 'checks if there is money on the balance' do
-    expect(subject.balance).to be > 0
+    expect(subject.balance).to eq(0)
   end
 
-  it 'adds money to the Oyster Card' do
-    current_balance = subject.balance
-    subject.top_up(OysterCard::FARE)
-    expect(subject.top_up).to be > current_balance
+  describe '#top_up' do
+
+  #  it 'recieves 1 argument' do
+   #   { subject.to respond_to(:top_up).with(1).argument }
+   # end
+
+    it 'can top up the balance' do
+      expect{ subject.top_up(OysterCard::FARE)}.to change{ subject.balance }.by OysterCard::FARE
+    end
+
+    it 'raise error if balance exceeds max capacity' do
+      subject.top_up(OysterCard::MAX_BALANCE)
+      expect { subject.top_up(1) }.to raise_error(OysterCard::ERROR[:max])
+    end
+  end
+  
+  describe '#touch_in' do
+
+    it 'checks if oyster card has less than the minimum fare (£1)' do
+      expect { subject.touch_in }.to raise_error(OysterCard::ERROR[:min])
+    end
+
+    describe 'top up card before tests' do
+      before(:each) do
+          subject.top_up(OysterCard::FARE)
+      end
+
+    it 'checks if oyster card has touched in' do
+      expect(subject.touch_in).to eq true
+    end
+
   end
 
-  it 'raise error if balance exceeds max capacity' do
-    expect { subject.top_up(OysterCard::MAX_BALANCE) }.to raise_error "You already have £#{OysterCard::MAX_BALANCE}!"
-  end
 
+  end
   it 'deducts money from the Oyster Card' do
-    current_balance = subject.balance
+    current_balance = subject.top_up(OysterCard::MAX_BALANCE)
     subject.deduct(OysterCard::FARE)
     expect(subject.deduct).to be < current_balance
   end
 
-  it 'checks if oyster card has touched in' do
-    subject.touch_in
-    expect(subject.touch_in).to eq true
-  end
+
 
   it 'checks if oyster card is in use' do
     expect(subject.in_journey?).to be(false)
@@ -38,20 +63,10 @@ describe OysterCard do
     expect(subject.balance).to be < current_balance
   end
 
-  it 'checks if oyster card has minimum of 1 pound' do
-    subject.touch_in
-    expect(subject.balance).to be >= OysterCard::MINIMUM_FARE
-  end
-
-  it 'checks if oyster card has less than the minimum fare (£1)' do
-    subject.deduct(balance)
-    expect(subject.touch_in).to raise_error 'insufficient funds'
-  end
-
   it 'checks if oyster reduces balance by correct amount' do
     expect { subject.touch_out }.to change { subject.balance }.by(OysterCard::MINIMUM_FARE)
   end
-
+ 
   # it 'stores the entry station on the card' do
   # subject.touch_in(:entry_station)
   # expect(subject.entry_station) to be
